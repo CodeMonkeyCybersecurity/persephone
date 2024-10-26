584395876d508e5657afe3cfce92242cddb6a4f2
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-10-26 19:17:14 +0800

    Update persephone.py

--- persephone.py
+++ persephone.py
@@ -2148,7 +2148,7 @@ def main():
                 '6': lambda: create_backup(config),  # Pass config to create_backup
                 '7': debug_borg_submenu,
                 '8': delete_archive_borg_submenu,
-                '9': diff_archives,
+                '9': diff_borg_submenu,
                 '10': export_tar,
                 '11': extract_borg_submenu,
                 '12': info_borg_submenu,
