e224a7252bd5eb151658a083e2cc8fdfd29ac7b7
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-11-10 20:46:27 +0800

    Update checkBorg.py

--- submodules/checkBorg.py
+++ submodules/checkBorg.py
@@ -1,12 +1,6 @@
-import subprocess
+import shutil
 import sys
 
-def check_borg_installed():
-    try:
-        subprocess.run(['borg', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    except (subprocess.CalledProcessError, FileNotFoundError):
-        print("Borg Backup is not installed. Please install it to proceed.")
-        sys.exit(1)
-
-# Call the function at the start of your script
-check_borg_installed()
+if shutil.which("borg") is None:
+    print("Error: Borg Backup is not installed. Please install it to proceed.")
+    sys.exit(1)
