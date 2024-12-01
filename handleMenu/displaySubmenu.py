0eb2084ff4439ed980d4e5d75a076048fae7a657
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-01 11:42:45 +0800

    Create displaySubmenu.py

---
+++ handleMenu/displaySubmenu.py
@@ -0,0 +1,13 @@
+# Display submenu
+def displaySubmenu():
+    """Display the submenu for the selected section."""
+    print("\n(0) Run backup with current configuration")
+    print("(1) Print out current configuration")
+    print("(2) Create new configuration")
+    print("(3) Edit existing configuration")
+    print("(4) Perform a dry run with current configuration")
+    print("(5) Delete current configuration")
+    print("(6) Replace current configuration")
+    print("(7) Return to main Menu")
+    print("(E) (E)xit")
+    logging.info("Displaying main submenu")
