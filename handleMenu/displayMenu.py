3d10158f24d9bbf26bd32ab188f86cee28d9c153
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-01 11:42:20 +0800

    Create displayMenu.py

---
+++ handleMenu/displayMenu.py
@@ -0,0 +1,17 @@
+# Display main menu
+def displayMenu():
+    """Display the main menu."""
+    print("Welcome to Code Monkey Cybersecurity's borgBackup.py.")
+    print("What would you like to do? Type the letter of the option you want and press enter:")
+    print("(M) Show this (M)enu")
+    print("(H) Show (H)elp")
+    print("(Y) Go to (Y)AML options")
+    print("(O) Go to repository (O)ptions")
+    print("(B) Go to (B)ackup options")
+    print("(R) Go to (R)estore options")
+    print("(D) Go to (D)ebugging options")
+    print("(A) Go to (A)rchive options")
+    print("(N) Run backup (N)ow")
+    print("(C) Add backup to crontab")
+    print("(E) (E)xit")
+    logging.info("Displaying main menu")
