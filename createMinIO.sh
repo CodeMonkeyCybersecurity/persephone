4a73091fed9c124f97f8c9628b2f17ab2f5a112d
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-02-07 11:48:34 +0800

    Delete createMinIO.sh
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- createMinIO.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# createMinIO.sh
-
-wget https://dl.min.io/server/minio/release/linux-amd64/minio
-chmod +x minio
-sudo mv minio /usr/local/bin/
-USER=$(whoami)
-sudo mkdir -p /data
-sudo chown -R $USER: /data && sudo chmod u+rxw /data
