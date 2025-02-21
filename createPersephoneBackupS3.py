05f2900365f555ae591aa331ddf423d2c14fb05a
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-02-21 14:12:00 +0800

    Update createPersephoneBackupS3.py
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- createPersephoneBackupS3.py
+++ createPersephoneBackupS3.py
@@ -150,7 +150,7 @@ def main():
     backup_cmd = [
         "sudo",
         "restic",
-        "-r", pers_repo_file,
+        "--repository-file", pers_repo_file,
         "--password-file", pers_pass_file,
         "backup",
         "--verbose",
@@ -165,7 +165,7 @@ def main():
     snapshots_cmd = [
         "sudo",
         "restic",
-        "-r", pers_repo_file,
+        "--repository-file", pers_repo_file,
         "--password-file", pers_pass_file,
         "snapshots"
     ]
